d31385a6a144df4109600bb1a7d52e195149ec38
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-05-17 16:27:03 -0400

    initial commit

---
+++ middle.js
@@ -0,0 +1,28 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (item in arr1) {
+    if (arr1[item] !== arr2[item]) return false;
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ [${arr1}] and [${arr2}] are two equal arrays`);
+  } else console.log(`❌❌❌ [${arr1}] and [${arr2}] are not equal arrays`);
+}
+
+
+function middle(arr) {
+  let newArr = [];
+  if (arr.length < 3) return newArr;
+  
+  if (arr.length % 2 === 0) newArr.push(arr[arr.length / 2] - 1, arr[(arr.length / 2)]);
+  else newArr.push(Math.floor(arr.length / 2) + 1);
+  return newArr;
+}
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2]), []);
