3ee36ed4957b4a0aacc3c1c6a79bfcbee580bc2a
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-05-17 09:56:33 -0400

    lint code

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
-const assertEqual = function(actual, expected){
-  let result = ""
-  if(actual === expected) result = `✅✅✅ Assertion passed: ${actual} === ${expected}`
-  else result = `❌❌❌ Assertion failed: ${actual} !== ${expected}`
-  console.log(result); 
-}
+const assertEqual = function(actual, expected) {
+  let result = "";
+  if (actual === expected) result = `✅✅✅ Assertion passed: ${actual} === ${expected}`;
+  else result = `❌❌❌ Assertion failed: ${actual} !== ${expected}`;
+  console.log(result);
+};
 
 
 assertEqual("Lighthouse Labs", "Bootcamp");
