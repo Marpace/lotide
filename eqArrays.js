ed51ef84fc87131fb5b2fedfdfef3afd1cf0a8a1
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-06-06 10:20:38 -0400

    modularized eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let result = "";
-  if (actual === expected) result = `✅✅✅ Assertion passed: ${actual} === ${expected}`;
-  else result = `❌❌❌ Assertion failed: ${actual} !== ${expected}`;
-  console.log(result);
-};
+const assertEqual = require("./assertEqual");
 
 function eqArrays(arr1, arr2) {
   if(arr1.length !== arr2.length) return false
@@ -13,8 +8,4 @@ function eqArrays(arr1, arr2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 6, 4], [1, 2, 4]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4, 5]), false);
-assertEqual(eqArrays([3, 2, 1, 5, 4], [1, 1, 1]), false);
-assertEqual(eqArrays([6, 4, 7], [6, 4, 7]), true);
+module.exports = eqArrays;
