13371865b3b8dd3030bd38dd8e1124d3f542b33c
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-06-06 10:44:33 -0400

    modularized middle.js

--- middle.js
+++ middle.js
@@ -1,17 +1,5 @@
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-}
-
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ [${arr1}] and [${arr2}] are two equal arrays`);
-  } else console.log(`❌❌❌ [${arr1}] and [${arr2}] are not equal arrays`);
-}
-
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 function middle(arr) {
   let newArr = [];
@@ -22,7 +10,4 @@ function middle(arr) {
   return newArr;
 }
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
